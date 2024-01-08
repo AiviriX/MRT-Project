15f40d8a6d9ab1b34ce3155fd8f53dc78baba18c
AivriX <81509337+AiviriX@users.noreply.github.com>
2024-01-08 23:54:42 +0800

    meow2

--- mrt-app/src/mrt-index.tsx
+++ mrt-app/src/mrt-index.tsx
@@ -7,12 +7,15 @@ import mrt from './res/station.jpg'
 import station from './res/station.jpg'
 
 
-
 const MRTIndex = () => {
     return (
-        <div> 
-            <div className="vignette w-full h-full">
-                <img src={mrt} alt="MRT"/>
+        <div className="relative">
+            <img src={station} alt="Background" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-white p-10">
+                <h1 className='text-4xl mb-4'>Hello!</h1>
+                <button className="border border-white text-white px-4 py-2 rounded bg-transparent">
+                    Get Started
+                </button>
             </div>
         </div>
     );
