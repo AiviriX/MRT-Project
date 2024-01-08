1baab21aadadfdb2aa348b86484307acaf0c4e6d
AivriX <81509337+AiviriX@users.noreply.github.com>
2024-01-08 23:55:12 +0800

    Revert "Station Index"
    This reverts commit 209706aa046c9bdc0e9832d557bd189d5e7cdb0e.

--- mrt-app/src/stations/station-index.tsx
+++ mrt-app/src/stations/station-index.tsx
@@ -13,10 +13,98 @@ const Stations = () => {
                 integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
                 crossOrigin="">
             </script>
+            <MapContainer center={[14.5047, 121.0297]} zoom={12} scrollWheelZoom={true}
+            className='box-border h-32 w-32 p-4 border-4 mx-3 my-3 pos-center'>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
 
-            <div className='border-box'>
+                {/*TODO Add to json late her */}
+                <Marker position={[14.652208254624389, 121.03234323138653]}>
+                    <Popup>
+                        North Avenue Station
+                    </Popup>
+                </Marker>
 
+                <Marker position={[14.643348953836682, 121.03796281550703]}>
+                    <Popup>
+                        Quezon Avenue Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.635222115280635, 121.04333937202267]}>
+                    <Popup>
+                        GMA Kamuning Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.619525829473252, 121.05111117373505]}>
+                    <Popup>
+                        Araneta Center Cubao Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.607496535782303, 121.05658583669512]}>
+                    <Popup>
+                        Santolan Annapolis Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.587884030596898, 121.05672494296502]}>
+                    <Popup>
+                        Ortigas Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.581807881398193, 121.05418292043099]}>
+                    <Popup>
+                        Shaw Boulevard Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.573784079170085, 121.04818553322576]}>
+                    <Popup>
+                        Boni Avenue Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.566931347769977, 121.04554092499848]}>
+                    <Popup>
+                        Guadalupe Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.55508836413639, 121.03439464706184]}>
+                    <Popup>
+                        Buendia Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.549573475275071, 121.02832335007369]}>
+                    <Popup>
+                        Ayala Station
+                    </Popup>
+                </Marker>
+
+                <Marker position={[14.542491312207575, 121.01941878138396]}>
+                    <Popup>
+                        Magallanes Station
+                    </Popup>    
+                </Marker>
+
+                <Marker position={[14.537699807729172, 121.0021913874413]}>
+                    <Popup>
+                        Taft Avenue Station
+                    </Popup>
+                </Marker>
+            </MapContainer>
+
+            <div className='box-border h-32 w-32 p-4 border-4 mx-3 my-3 pos-center'>
+                
             </div>
+
+
         </div>
     );
 
