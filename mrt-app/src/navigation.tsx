62a3e0ec66c4fefcf5bccb0d9f0def10d5f1e5b5
AivriX <81509337+AiviriX@users.noreply.github.com>
2024-02-14 05:55:50 +0800

    Basic navbar responsiveness

--- mrt-app/src/navigation.tsx
+++ mrt-app/src/navigation.tsx
@@ -1,17 +1,17 @@
 import './style/mrt_index.css'
 import './style/nav.css'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-
 import { removeSessionToken, hasSessionToken } from './auth/sessionTokenManager'
 
 export const Navigation = () => {
     const [hasToken, setToken] = useState(hasSessionToken())
+    const [showDropdown, setShowDropdown] = useState(false);
 
-    useEffect(() => {
-        
-    }, [hasToken])
+    const toggleDropdown = () => {
+        setShowDropdown(!showDropdown);
+    };
 
     //Handles the instructions to remove the session token locally....
     const locRemoveSessionToken = () => {
@@ -19,34 +19,41 @@ export const Navigation = () => {
         setToken(hasSessionToken());
     }
 
-
     return (   
-            <nav className='flex shrink bg-white border-gray-200 dark:bg-gray-900 top-0 z-10 relative'>
-                <ul className='z-19 sticky list-none m-0 p-0 overflow-hidden bg-blue-900 text-gray-200 fixed w-full'>
-                    <li className='left'>
-                        <Link className="shrink w-32" to="/">MRT TMS</Link>
-                    </li>
-                    <li className='right'>
-                        <Link to="/stations/mrt3">Stations</Link>
-                    </li>
-
-                    {
-                    (hasToken) ? (
-                        <div>
-                            <li onClick={locRemoveSessionToken} className='flex right '>
-                                <Link to='/'> Logout </Link>
-                            </li>
-                            <li className='flex right'>
-                                <Link to="/admin/control"> Admin Control </Link>
-                            </li>
-                        </div>                    
-                    ) : (
-                        <li className='flex right'>
-                            <Link to="/login/admin"> Login </Link>
-                        </li>
-                    ) }
-                </ul>
-            </nav>
+        <nav className=''>
+            <ul className="">
+                <div className='flex justify-items-start'>
+                <li className=''>
+                    <Link className="flex mx-10 text-[#fff] font-bold " to="/">MRT TMS</Link>
+                </li>
+                </div>
+                <div className='nav-list'>
+                <li className='nav-item'>
+                    <a className="nav-link" onClick={toggleDropdown}>Menu</a>
+                    {showDropdown && (
+                        <div className="dropdown">
+                            <ul>
+                                <li>
+                                    {hasToken ? (
+                                        <Link className="nav-link" to="/admin/control">Admin Control</Link>
+                                    ) : (
+                                        <Link className="" to="/login/admin">Login</Link>
+                                    )}
+                                </li>
+                                {hasToken && (
+                                    <li onClick={locRemoveSessionToken}>
+                                        <Link className="nav-link" to='/'>Logout</Link>
+                                    </li>
+                                )}
+                            </ul>
+                            
+                        </div>
+                    )}
+                </li>
+                </div>
+            </ul>
+        </nav>
     );
 }
+
 export default Navigation
